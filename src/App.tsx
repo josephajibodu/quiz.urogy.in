dfd1fd02f144c9c0120fbeadd93790e64c6a8a12
cremirdevio <josephajibodu@gmail.com>
2023-12-02 07:30:22 +0100

    landing page design completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-      <div className="relative min-h-screen bg-gradient-to-b from-[#BDDAFF] to-white">
-          {/* <svg className="absolute right-0" width="415" height="539" viewBox="0 0 415 539" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div className="relative min-h-screen bg-gradient-to-b from-[#BDDAFF] to-white">
+      <svg className="absolute right-0" width="415" height="539" viewBox="0 0 415 539" fill="none" xmlns="http://www.w3.org/2000/svg">
               <circle cx="344" cy="195" r="75.5" stroke="#00286B" strokeOpacity="0.15"/>
               <circle cx="344" cy="195" r="111.5" stroke="#00286B" strokeOpacity="0.15"/>
               <circle cx="344" cy="195" r="157.74" stroke="#00286B" strokeOpacity="0.15"/>
@@ -18,32 +18,53 @@ function App() {
             <circle cx="-1" cy="344" r="198.5" stroke="#00286B" strokeOpacity="0.15"/>
             <circle cx="-1" cy="344" r="292.5" stroke="#00286B" strokeOpacity="0.15"/>
             <circle cx="-1" cy="344" r="343.5" stroke="#00286B" strokeOpacity="0.15"/>
-        </svg> */}
-        <main className="flex flex-col items-center">
-          <header className="h-24 w-full border-b-brand border flex justify-center items-center">
-            <div className="font-bold text-lg">urogy.in</div>
-          </header>
+        </svg>
+      <main className="flex flex-col items-center pb-16">
+        <header className="h-24 w-full border-b-brand border flex justify-center items-center">
+          <div className="font-bold text-lg">urogy.in</div>
+        </header>
 
-          <h1 className="text-center text-5xl lg:text-8xl mt-20 lg:mt-40 leading-[4rem] lg:leading-[6.4rem]">
-            <div>Urogynecology</div>
-            <div className="w-fit flex items-center gap-4 lg:gap-14">
-              Patient 
-              <span className="block lg:hidden border-4 border-accent rounded-full w-[120px] lg:w-[200px] h-10"></span>
-              <span className="hidden lg:block border-8 border-accent rounded-full w-[500px] h-20"></span>
-            </div>
-            <div className="w-fit flex items-center gap-4 lg:gap-8 relative">
-              <span className="hidden lg:block border-8 bg-accent border-accent rounded-full w-[150px] h-20"></span>
-              <span className="block lg:hidden border-8 bg-accent border-accent rounded-full w-[50px] h-10 absolute top-[100%]"></span>
-              <span>Education App</span>
-            </div>
-          </h1>
-
-          <div className="">
-
+        <h1 className="text-center text-5xl lg:text-8xl mt-20 lg:mt-40 leading-[4rem] lg:leading-[6.4rem]">
+          <div>Urogynecology</div>
+          <div className="w-fit flex items-center gap-4 lg:gap-14">
+            Patient
+            <span className="block lg:hidden border-4 border-accent rounded-full w-[120px] lg:w-[200px] h-10"></span>
+            <span className="hidden lg:block border-8 border-accent rounded-full w-[500px] h-20"></span>
           </div>
-        </main>
-      </div>
-  )
+          <div className="w-fit flex items-center gap-4 lg:gap-8 relative">
+            <span className="hidden lg:block border-8 bg-accent border-accent rounded-full w-[150px] h-20"></span>
+            <span className="block lg:hidden border-8 bg-accent border-accent rounded-full w-[50px] h-10 absolute top-[100%]"></span>
+            <span>Education App</span>
+          </div>
+        </h1>
+
+        <div className="aspect-video border-brand border-2 h-[600px] rounded-3xl mt-28 relative">
+            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] cursor-pointer">
+              <div className="bg-accent/40 rounded-full flex justify-center items-center animate-ping absolute top-0 w-24 h-24">
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-24 h-24 text-brand pl-2 flex justify-center items-center cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                />
+              </svg>
+            </div>
+        </div>
+
+        <button className="border-brand border-2 mt-16 rounded-full h-[56px] px-14 hover:bg-accent hover:text-white active:bg-brand">
+          Start the Questionnaire
+        </button>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
