cc92bcb91c99c511809f78fa094473fd3067d228
cremirdevio <josephajibodu@gmail.com>
2023-12-02 05:14:40 +0100

    eslint issue resolved

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -17,11 +14,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-  },
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
   },
 }
