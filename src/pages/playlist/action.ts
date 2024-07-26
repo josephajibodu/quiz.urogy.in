a4394e9bf4fcbc117ccc911efa40411b4ca61abd
cremirdevio <josephajibodu@gmail.com>
2024-07-26 10:29:34 +0100

    playlist fix

--- src/pages/playlist/action.ts
+++ src/pages/playlist/action.ts
@@ -3,19 +3,9 @@ import { ActionFunction } from "react-router"
 
 export interface ActionData { action: 'submit-quiz' | 'generate-playlist', responses?: number[], invitation_code: string }
 
-function promiseTimeout(milliseconds: number) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve('Timeout finished!');
-        }, milliseconds);
-    });
-}
-
 
 const PlaylistPageAction: ActionFunction = async ({ request }) => {
     const data = await request.json() as ActionData
-
-    await promiseTimeout(10_000);
 
     return await generatePlaylist(data)
 }
